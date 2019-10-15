3fe9324c6c92f136f54420aca30b32b432460189
mo3az.abdellatif <mo3az.abdellatif@gmail.com>
2019-10-15 16:54:57 +0200

    done

--- js/main.js
+++ js/main.js
@@ -19,6 +19,37 @@ window.onload = function () {
     $('.selectpicker').selectpicker({
         dropupAuto: false
     });
+
+    //map
+    var adresse = "<img style='width:50px; text-align: left; display:inline-block; margin-right: 10px; vertical-align: sub;' src='img/logofooter.png'> <div style='display:inline-block;'>Blackstone<br>0540000000<br>info@blackstone.sa</div>";
+
+
+    var location = [adresse[0], $("#map-cont").attr("lat"), $("#map-cont").attr("long")];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 11,
+        center: new google.maps.LatLng(24.8629889, 46.5980617),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        zoomControl: false,
+        fullscreenControl: false
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker;
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(location[1], location[2]),
+        map: map,
+        icon: 'images/pin.png',
+
+    });
+
+    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+        return function () {
+            infowindow.setContent(location[0]);
+            infowindow.open(map, marker);
+        }
+    })(marker));
 }
 $(document).ready(function () {
     $('.selectpicker').selectpicker({
@@ -164,36 +195,6 @@ $(document).ready(function () {
             },
         }
     });
-    //map
-    var adresse = "<img style='width:50px; text-align: left; display:inline-block; margin-right: 10px; vertical-align: sub;' src='img/logofooter.png'> <div style='display:inline-block;'>Blackstone<br>0540000000<br>info@blackstone.sa</div>";
-
-
-    var location = [adresse[0], $("#map").attr("lat"), $("#map").attr("long")];
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 11,
-        center: new google.maps.LatLng(24.8629889, 46.5980617),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoomControl: false,
-        fullscreenControl: false
-    });
-
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker;
-    marker = new google.maps.Marker({
-        position: new google.maps.LatLng(location[1], location[2]),
-        map: map,
-        icon: ' ',
-
-    });
-
-    google.maps.event.addListener(marker, 'click', (function (marker, i) {
-        return function () {
-            infowindow.setContent(location[0]);
-            infowindow.open(map, marker);
-        }
-    })(marker));
 
     ////////////////////////////////////////////
     $('.checkmark').click(function () {
@@ -201,6 +202,9 @@ $(document).ready(function () {
         $('input[name=radio]').removeAttr('checked');
         $('input' + '#' + x).attr("checked", "checked");
     });
+
+
+
 
 
 
